2ac07023fceb695b672afb089e387ec37e03a811
inigo hernandez <ihernandezzarraga@gmail.com>
2019-12-03 08:19:50 +0100

    comit final

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('.dataTable').DataTable();
+});
+
+
+function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+
+function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+}
